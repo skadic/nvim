6145ed47aa4713a3a92a66d17217eb7cbdb568f6
skadic <me@skadic.moe>
2025-06-24 20:34:06 +0200

    Update blink 0.13

--- lua/basic/lazy/plugins/ui.lua
+++ lua/basic/lazy/plugins/ui.lua
@@ -1,9 +1,9 @@
 return {
-  { 'echasnovski/mini.files', version = false, opts = {} },
+	{ "echasnovski/mini.files", version = false, opts = {} },
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
-    enabled = false,
+		enabled = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			--"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -35,6 +35,33 @@ return {
 		cmd = "Neotree",
 		event = "BufEnter",
 		opts = require("basic.neotree"),
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+		},
 	},
 	{
 		"j-hui/fidget.nvim",
