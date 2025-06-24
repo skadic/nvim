31740c3f557e8c82e9c69d33499aa30221292d8d
skadic <me@skadic.moe>
2025-06-24 20:34:06 +0200

    Remove old Config

--- lua/skadic/lsp/settings/elixir-ls.lua
+++
@@ -1,5 +0,0 @@
-return {
-  settings = {
-    filetypes = { "exs" },
-  },
-}
--- lua/skadic/neovide.lua
+++
@@ -1,4 +0,0 @@
-if vim.g.neovide then
-	--vim.api.nvim_command("set guifont=FiraCode\\ Nerd\\ Font\\ Mono:h10")
-	vim.api.nvim_command("set guifont=agave\\ Nerd\\ Font\\ Mono:h16")
-end
